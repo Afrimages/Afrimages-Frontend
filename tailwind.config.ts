da4fd8b8975ba99387cb6d2c89c662e598fc3f58
David Enikuomehin <enikuauth@gmail.com>
2023-12-02 19:26:29 +0100

    chore: import required assets

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,29 +1,38 @@
 import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        poppins: ['var(--font-poppins)'],
-      },
-      colors: {
-        green500: '#A9DCB3',
-        green800: '#459A56',
-        green1000: '#295C34',
-        green1100: '#1B3D22',
-        orange800: '#E76020',
-        orange900: '#C0501A',
-      },
-      spacing: {
-        xPadding: '5.2%',
-      },
+    content: [
+        './pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './components/**/*.{js,ts,jsx,tsx,mdx}',
+        './app/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                poppins: ['var(--font-poppins)'],
+                lato: ['var(--font-lato)'],
+            },
+            colors: {
+                green500: '#A9DCB3',
+                green700: '#53B968',
+                green800: '#459A56',
+                green1000: '#295C34',
+                green1100: '#1B3D22',
+
+                orange400: '#FBE4D9',
+                orange700: '#EB7A45',
+                orange800: '#E76020',
+                orange900: '#C0501A',
+
+                grey100: '#FAF9F8',
+                grey500: '#BDB4AD',
+                grey600: '#7D736C',
+            },
+            spacing: {
+                xPadding: '5.2%',
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
 export default config;
