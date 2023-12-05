a962ce1a9aef233eccb68d4c20726e552eb83e67
David Enikuomehin <enikuauth@gmail.com>
2023-12-05 14:00:28 +0100

    feat(upload): create logic for preview inputted content

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -1,3 +1,6 @@
+'use client';
+
+import { useState } from 'react';
 import Image from 'next/image';
 
 import {
@@ -12,103 +15,150 @@ import videoIcon from '@/public/svg/video.svg';
 import imageIcon from '@/public/svg/image.svg';
 import collectionIcon from '@/public/svg/collection.svg';
 import uploadIcon from '@/public/svg/upload.svg';
+import eyeIcon from '@/public/svg/eye.svg';
+import lockIcon from '@/public/svg/lock.svg';
 
 export default function Upload() {
+    const [active, setActive] = useState(false);
+    const [selectedFile, setSelectedFile] = useState<any>(null);
+    const [formData, setFormData] = useState({
+        userTitle: '',
+        userDetails: '',
+        userTools: '',
+        userImgCatog: '',
+        userVisibilty: '',
+        userLicense: '',
+        userRating: false,
+        userPermission: false,
+        userAgree: false,
+    });
+
+    const handleFileInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const file = e.target.files && e.target.files[0];
+
+        // Set the selected file to state
+        setSelectedFile(file);
+
+        setActive(true);
+    };
+
+    const handleInputChange = (name: string, value: string | boolean) => {
+        setFormData({
+            ...formData,
+            [name]: value,
+        });
+    };
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        console.log('Form Submitted');
+    };
+
     return (
         <main className='px-6 bg-grey100 rounded-[10px] font-lato lg:grid lg:grid-cols-[70%_30%] lg:gap-6 lg:px-[100px] lg:py-8'>
-            <div className='text-center flex flex-col items-center justify-center gap-7 bg-white min-h-[90dvh] '>
-                <div>
-                    <h1 className='font-bold text-[1.75rem] leading-[2.5rem]'>
-                        Upload Content
-                    </h1>
+            {!active ? (
+                <div className='text-center flex flex-col items-center justify-center gap-7 bg-white min-h-[90dvh] '>
+                    <div>
+                        <h1 className='font-bold text-[1.75rem] leading-[2.5rem]'>
+                            Upload Content
+                        </h1>
 
-                    <p className='text-orange700 text-sm leading-6 font-medium'>
-                        Upload the Image you want to share on afrimage
-                    </p>
+                        <p className='text-orange700 text-sm leading-6 font-medium'>
+                            Upload the Image you want to share on afrimage
+                        </p>
+                    </div>
+
+                    <div className='flex items-center justify-center gap-6'>
+                        {[
+                            {
+                                label: 'Video',
+                                name: 'user-video',
+                                accept: 'video/*',
+                                multiple: false,
+                                iconUrl: videoIcon,
+                            },
+                            {
+                                label: 'Image',
+                                name: 'user-img',
+                                accept: 'image/*',
+                                multiple: false,
+                                iconUrl: imageIcon,
+                            },
+                            {
+                                label: 'Collection',
+                                name: 'user-collection',
+                                multiple: true,
+                                iconUrl: collectionIcon,
+                            },
+                        ].map((item) => (
+                            <div key={item.label} className='w-fit'>
+                                <label
+                                    htmlFor={item.name}
+                                    className='bg-orange400 rounded-xl w-[4.25rem] h-[4.25rem] flex items-center justify-center cursor-pointer'
+                                >
+                                    <Image src={item.iconUrl} alt='' />
+
+                                    <input
+                                        type='file'
+                                        name={item.name}
+                                        id={item.name}
+                                        accept={item.accept || undefined}
+                                        multiple={item.multiple}
+                                        onChange={handleFileInputChange}
+                                        className='hidden'
+                                    />
+                                </label>
+
+                                <p className='text-orange700 mt-1 font-medium text-sm leading-6'>
+                                    {item.label}
+                                </p>
+                            </div>
+                        ))}
+                    </div>
                 </div>
+            ) : null}
 
-                <div className='flex items-center justify-center gap-6'>
-                    {[
-                        {
-                            label: 'Video',
-                            name: 'user-video',
-                            accept: 'video/*',
-                            multiple: false,
-                            iconUrl: videoIcon,
-                        },
-                        {
-                            label: 'Image',
-                            name: 'user-img',
-                            accept: 'image/*',
-                            multiple: false,
-                            iconUrl: imageIcon,
-                        },
-                        {
-                            label: 'Collection',
-                            name: 'user-collection',
-                            multiple: true,
-                            iconUrl: collectionIcon,
-                        },
-                    ].map((item) => (
-                        <div key={item.label} className='w-fit'>
-                            <label
-                                htmlFor={item.name}
-                                className='bg-orange400 rounded-xl w-[4.25rem] h-[4.25rem] flex items-center justify-center cursor-pointer'
-                            >
-                                <Image src={item.iconUrl} alt='' />
-
-                                <input
-                                    type='file'
-                                    name={item.name}
-                                    id={item.name}
-                                    accept={item.accept || undefined}
-                                    multiple={item.multiple}
-                                    className='hidden'
-                                />
-                            </label>
-
-                            <p className='text-orange700 mt-1 font-medium text-sm leading-6'>
-                                {item.label}
-                            </p>
-                        </div>
-                    ))}
-                </div>
-            </div>
+            {/* This section displays data as it's inputted in the Form  */}
+            {active ? (
+                <InputDisplay formData={formData} selectedFile={selectedFile} />
+            ) : null}
 
             <form
-                action=''
+                onSubmit={handleSubmit}
                 className='mt-6 pb-8 space-y-4 lg:bg-white lg:mt-0 lg:py-7 lg:px-5'
             >
                 <div className='grid gap-4 md:grid-cols-2 mb-4 lg:grid-cols-1'>
                     {[
-                        { label: 'Image Title', name: 'user-title' },
+                        { label: 'Image Title', name: 'userTitle' },
                         {
                             label: 'Short details about the image',
-                            name: 'user-details',
+                            name: 'userDetails',
                         },
-                        { label: 'Tools Used', name: 'user-tools' },
+                        { label: 'Tools Used', name: 'userTools' },
                     ].map((item) => (
                         <InputText
                             key={item.label}
                             label={item.label}
                             name={item.name}
+                            onChange={handleInputChange}
                         />
                     ))}
 
                     {[
                         {
                             label: 'Image Categories',
-                            name: 'user-img-catog',
+                            name: 'userImgCatog',
                             options: ['Select a category', 'Food', 'Travel'],
                         },
                         {
                             label: 'Afrimage Visibility',
-                            name: 'user-visibility',
+                            name: 'userVisibility',
                             options: ['Select an option', 'Everyone', 'Nobody'],
                         },
                         {
                             label: 'License',
-                            name: 'user-license',
+                            name: 'userLicense',
                             options: ['Select an option', 'Free', 'Paid'],
                         },
                     ].map((item) => (
@@ -117,6 +167,7 @@ export default function Upload() {
                             label={item.label}
                             name={item.name}
                             options={item.options}
+                            onChange={handleInputChange}
                         />
                     ))}
                 </div>
@@ -124,23 +175,24 @@ export default function Upload() {
                 {[
                     {
                         label: 'This image contains Adult content',
-                        name: 'user-rating',
+                        name: 'userRating',
                     },
                     {
                         label: 'I upload image I own the right to',
-                        name: 'user-permission',
+                        name: 'userPermission',
                     },
                 ].map((item) => (
                     <InputCheckbox
                         key={item.label}
                         label={item.label}
                         name={item.name}
+                        onChange={handleInputChange}
                     />
                 ))}
 
                 {/* Custom Checkbox comp */}
                 <div className='flex items-center justify-between text-lg font-medium leading-7 py-1 gap-4'>
-                    <label htmlFor='user-agree' className='text-black'>
+                    <label htmlFor='userAgree' className='text-black'>
                         I Agree to all{' '}
                         <a href='' className='text-green700'>
                             Afrimage content policy
@@ -149,9 +201,12 @@ export default function Upload() {
 
                     <input
                         type='checkbox'
-                        name='user-agree'
-                        id='user-agree'
-                        className='h-[18px] w-[18px] border-2 border-[#49454F] bg-transparent'
+                        name='userAgree'
+                        id='userAgree'
+                        onChange={(e) =>
+                            handleInputChange('userAgree', e.target.checked)
+                        }
+                        className='form-checkbox h-[18px] w-[18px] border-2 border-[#49454F] bg-transparent cursor-pointer rounded-sm checked:bg-orange700 focus:bg-orange700 focus:outline-orange700 checked:hover:bg-orange700 checked:focus:bg-orange700'
                     />
                 </div>
 
@@ -161,7 +216,7 @@ export default function Upload() {
                         type='main'
                         className='flex items-center justify-center gap-2'
                     >
-                        <Image src={uploadIcon} alt='' />
+                        <Image src={uploadIcon} alt='' width={24} height={24} />
                         Publish
                     </FormButton>
 
@@ -171,5 +226,68 @@ export default function Upload() {
                 </div>
             </form>
         </main>
+    );
+}
+
+type InputDisplayProps = {
+    formData: {
+        userDetails: string;
+        userImgCatog: string;
+        userTitle: string;
+        // userVisibility: string;
+        // userLicense: string;
+    };
+    selectedFile: any;
+};
+
+function InputDisplay({ formData, selectedFile }: InputDisplayProps) {
+    return (
+        <section className='bg-white  py-8 px-2 rounded-[10px] flex flex-col justify-end gap-4 md:px-9 md:py-6 lg:gap-2 h-full'>
+            <Image
+                src={selectedFile}
+                alt='Selected File'
+                className='bg-red-400 flex-1'
+            />
+
+            <div className='leading-6'>
+                <h3 className='text-sm text-grey600 leading-6 font-medium'>
+                    Title
+                </h3>
+                <p className='text-black font-bold min-h-[24px]'>
+                    {formData.userTitle}
+                </p>
+            </div>
+
+            <div className='leading-6'>
+                <h3 className='text-sm text-grey600 leading-6 font-medium'>
+                    Details of the image
+                </h3>
+                <p className='text-black font-bold min-h-[24px]'>
+                    {formData.userDetails}
+                </p>
+            </div>
+
+            <div className='leading-6'>
+                <h3 className='text-sm text-grey600 leading-6 font-medium'>
+                    Image Categories
+                </h3>
+                <p className='text-black font-bold min-h-[24px]'>
+                    {formData.userImgCatog}
+                </p>
+            </div>
+
+            <div className='flex items-center gap-9 font-medium text-lg leading-7'>
+                <div className='flex items-center gap-1'>
+                    {/* TODO: Set Logic to change Icon and text based on user license */}
+                    <Image src={lockIcon} alt='' width={24} height={24} />
+                    <p className='text-success'>Free</p>
+                </div>
+                <div className='flex items-center gap-1'>
+                    {/* TODO: Set Logic to change icon and text based on user Visibility */}
+                    <Image src={eyeIcon} alt='' width={24} height={24} />
+                    <p className='text-grey900'>Everyone</p>
+                </div>
+            </div>
+        </section>
     );
 }
